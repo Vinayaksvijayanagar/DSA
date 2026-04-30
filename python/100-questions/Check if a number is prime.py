2fdc889acfde35c331e1d439275d51ca9b90fa20
Vinayak Vijayanagar <rani.vini.viddi.sakshi@gmail.com>
2026-04-30 20:31:35 +0530

    hi

---
+++ python/100-questions/Check if a number is prime.py
@@ -0,0 +1,12 @@
+#Check if a number is prime
+n = int(input())
+if n<2 :
+    print("not prime")
+else:
+    for i in range(2,int((n**0.5+1))):
+        if n%i == 0:
+            print("not a prime")
+            break
+    else:
+        print("prime")
+        
